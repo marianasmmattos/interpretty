e9c4eb2b86fcdc4ff3e8fff2088f7c9f862db4e6
Mari <mareaceae@gmail.com>
2023-09-28 14:58:47 -0300

    feat: improve benchmarking visualization

--- benchmark.js
+++ benchmark.js
@@ -3,6 +3,23 @@ const Benchmark = require('benchmark');
 const { main: classBased } = require('./class-based/main');
 const { main: recursive } = require('./class-recursive/main')
 
+function suiteHelper() {
+  console.log(``)
+  console.log('-------------------------------------')
+  console.log(``)
+  console.log(chalk.bgGreen('Suite Results:'));
+  console.log(``)
+  this.forEach(result => {
+    console.log(chalk.bgYellow(`${result.name}:`))
+    console.log(``)
+    console.log(chalk.red(`${result.hz}`), 'ops/sec')
+    console.log(chalk.red(`${result.count}`), 'times')
+    console.log(chalk.red(`${result.stats.moe}`), 'or', chalk.red(`${result.stats.rme}`), 'margin of errors')
+    console.log(chalk.red(`${result.stats.variance}`), 'variance')
+    console.log(``)
+  })
+}
+
 const suite = new Benchmark.Suite;
 
 suite
@@ -12,20 +29,5 @@ suite
   .add('Recursive class', () => {
     recursive()
   })
-  .on('complete', function() {
-    console.log(``)
-    console.log('-------------------------------------')
-    console.log(``)
-    console.log(chalk.bgGreen('Suite Results:'));
-    console.log(``)
-    this.forEach(result => {
-      console.log(chalk.bgYellow(`${result.name}:`))
-      console.log(``)
-      console.log(chalk.red(`${result.hz}`), 'ops/sec')
-      console.log(chalk.red(`${result.count}`), 'times')
-      console.log(chalk.red(`${result.stats.moe}`), 'or', chalk.red(`${result.stats.rme}`), 'margin of errors')
-      console.log(chalk.red(`${result.stats.variance}`), 'variance')
-      console.log(``)
-    });
-  })
+  .on('complete', suiteHelper)
   .run({ async: true });
